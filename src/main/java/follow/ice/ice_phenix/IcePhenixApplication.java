1c36c6bd240543f88f345e9f2d6858027d838f45
follow.ice <follow.ice.phenix@outlook.com>
2020-03-18 22:06:30 +0800

    init project

---
+++ src/main/java/follow/ice/ice_phenix/IcePhenixApplication.java
@@ -0,0 +1,13 @@
+package follow.ice.ice_phenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcePhenixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcePhenixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/follow/ice/ice_phenix/IcePhenixApplicationTests.java
@@ -0,0 +1,13 @@
+package follow.ice.ice_phenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcePhenixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
